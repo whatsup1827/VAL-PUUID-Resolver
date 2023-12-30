408e6d4e922f6434e91ce7e269be3a142069006b
whatsup1827 <whatsup1827@ANTHONY-PC>
2023-12-30 16:38:49 -0500

    Add project files.

---
+++ valpuuidresolver/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        TextInput = New TextBox()
+        TextOutput = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 85)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(328, 38)
+        Button1.TabIndex = 0
+        Button1.Text = "Submit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(104, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(132, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter a Riot ID or PUUID"
+        ' 
+        ' TextInput
+        ' 
+        TextInput.Location = New Point(12, 27)
+        TextInput.Name = "TextInput"
+        TextInput.Size = New Size(328, 23)
+        TextInput.TabIndex = 2
+        ' 
+        ' TextOutput
+        ' 
+        TextOutput.Location = New Point(12, 56)
+        TextOutput.Name = "TextOutput"
+        TextOutput.ReadOnly = True
+        TextOutput.Size = New Size(328, 23)
+        TextOutput.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AcceptButton = Button1
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(352, 141)
+        Controls.Add(TextOutput)
+        Controls.Add(TextInput)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        ShowIcon = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "VALORANT PUUID Resolver"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextInput As TextBox
+    Friend WithEvents TextOutput As TextBox
+
+End Class
---
+++ valpuuidresolver/Form1.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Net.Http
+Imports Newtonsoft.Json.Linq
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Left = (Me.ClientSize.Width - Label1.Width) / 2
+    End Sub
+
+    Private Async Function GetValorantPUUIDAsync(playerName As String, playerTag As String) As Task(Of String)
+        Using client As New HttpClient()
+            Dim url As String = $"https://api.henrikdev.xyz/valorant/v1/account/{playerName}/{playerTag}"
+            Try
+                Dim response As HttpResponseMessage = Await client.GetAsync(url)
+                If response.IsSuccessStatusCode Then
+                    Dim responseBody As String = Await response.Content.ReadAsStringAsync()
+                    Dim json As JObject = JObject.Parse(responseBody)
+                    Return json("data")("puuid").ToString()
+                Else
+                    Return "Error: " & response.ReasonPhrase
+                End If
+            Catch e As Exception
+                Return "Exception: " & e.Message
+            End Try
+        End Using
+    End Function
+
+    Private Async Function GetValorantUsernameAsync(puuid As String) As Task(Of String)
+        Using client As New HttpClient()
+            Dim url As String = $"https://api.henrikdev.xyz/valorant/v1/by-puuid/account/{puuid}?force=true"
+            Try
+                Dim response As HttpResponseMessage = Await client.GetAsync(url)
+                If response.IsSuccessStatusCode Then
+                    Dim responseBody As String = Await response.Content.ReadAsStringAsync()
+                    Dim json As JObject = JObject.Parse(responseBody)
+                    Return $"{json("data")("name")}#{json("data")("tag")}"
+                Else
+                    Return "Error: " & response.ReasonPhrase
+                End If
+            Catch e As Exception
+                Return "Exception: " & e.Message
+            End Try
+        End Using
+    End Function
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim input As String = TextInput.Text.Trim()
+        Dim output As String = String.Empty
+
+        ' Regex pattern to match UUID format
+        Dim uuidPattern As String = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+
+        If input.Contains("#") Then
+            Dim parts As String() = input.Split("#"c)
+            If parts.Length = 2 Then
+                output = Await GetValorantPUUIDAsync(parts(0), parts(1))
+            Else
+                output = "Invalid input format for username."
+            End If
+        ElseIf Regex.IsMatch(input, uuidPattern) Then
+            output = Await GetValorantUsernameAsync(input)
+        Else
+            output = "Invalid input format for PUUID."
+        End If
+
+        TextOutput.Text = output
+    End Sub
+End Class
