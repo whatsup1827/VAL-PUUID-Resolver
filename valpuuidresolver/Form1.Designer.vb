b11daf4f94fb90ba4ca0ca8676a83d54174cfc02
whatsup1827 <whatsup1827@gmail.com>
2024-05-21 20:49:43 -0400

    Add support for API key

--- valpuuidresolver/Form1.Designer.vb
+++ valpuuidresolver/Form1.Designer.vb
@@ -22,26 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Button1 = New Button()
+        Submit = New Button()
         Label1 = New Label()
         TextInput = New TextBox()
         TextOutput = New TextBox()
-        Button2 = New Button()
+        Copy = New Button()
+        ApiKeyInput = New TextBox()
+        Label2 = New Label()
+        Label3 = New Label()
         SuspendLayout()
         ' 
-        ' Button1
+        ' Submit
         ' 
-        Button1.Location = New Point(12, 85)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(160, 32)
-        Button1.TabIndex = 1
-        Button1.Text = "Submit"
-        Button1.UseVisualStyleBackColor = True
+        Submit.Location = New Point(14, 170)
+        Submit.Name = "Submit"
+        Submit.Size = New Size(160, 32)
+        Submit.TabIndex = 1
+        Submit.Text = "Submit"
+        Submit.UseVisualStyleBackColor = True
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(104, 9)
+        Label1.Location = New Point(14, 9)
         Label1.Name = "Label1"
         Label1.Size = New Size(132, 15)
         Label1.TabIndex = 1
@@ -56,32 +59,60 @@ Partial Class Form1
         ' 
         ' TextOutput
         ' 
-        TextOutput.Location = New Point(12, 56)
+        TextOutput.Location = New Point(12, 132)
         TextOutput.Name = "TextOutput"
         TextOutput.ReadOnly = True
         TextOutput.Size = New Size(328, 23)
         TextOutput.TabIndex = 3
         ' 
-        ' Button2
+        ' Copy
         ' 
-        Button2.Location = New Point(180, 85)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(160, 32)
-        Button2.TabIndex = 2
-        Button2.Text = "Copy"
-        Button2.UseVisualStyleBackColor = True
+        Copy.Location = New Point(180, 170)
+        Copy.Name = "Copy"
+        Copy.Size = New Size(160, 32)
+        Copy.TabIndex = 2
+        Copy.Text = "Copy"
+        Copy.UseVisualStyleBackColor = True
+        ' 
+        ' ApiKeyInput
+        ' 
+        ApiKeyInput.Location = New Point(12, 77)
+        ApiKeyInput.Name = "ApiKeyInput"
+        ApiKeyInput.Size = New Size(328, 23)
+        ApiKeyInput.TabIndex = 4
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(14, 59)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(47, 15)
+        Label2.TabIndex = 5
+        Label2.Text = "API Key"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(14, 114)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(45, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Output"
         ' 
         ' Form1
         ' 
-        AcceptButton = Button1
+        AcceptButton = Submit
         AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(352, 126)
-        Controls.Add(Button2)
-        Controls.Add(TextOutput)
-        Controls.Add(TextInput)
+        ClientSize = New Size(352, 215)
         Controls.Add(Label1)
-        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label3)
+        Controls.Add(TextInput)
+        Controls.Add(ApiKeyInput)
+        Controls.Add(TextOutput)
+        Controls.Add(Submit)
+        Controls.Add(Copy)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         MinimizeBox = False
@@ -93,10 +124,13 @@ Partial Class Form1
         PerformLayout()
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Submit As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents TextInput As TextBox
     Friend WithEvents TextOutput As TextBox
-    Friend WithEvents Button2 As Button
+    Friend WithEvents Copy As Button
+    Friend WithEvents ApiKeyInput As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 
 End Class
--- valpuuidresolver/Form1.vb
+++ valpuuidresolver/Form1.vb
@@ -3,13 +3,18 @@ Imports Newtonsoft.Json.Linq
 Imports System.Text.RegularExpressions
 
 Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label1.Left = (Me.ClientSize.Width - Label1.Width) / 2
-    End Sub
+    Private Async Function GetValorantPUUIDAsync(playerName As String, playerTag As String, apiKey As String) As Task(Of String)
+        If String.IsNullOrWhiteSpace(apiKey) Then
+            Return "Error: No API key provided."
+        End If
 
-    Private Async Function GetValorantPUUIDAsync(playerName As String, playerTag As String) As Task(Of String)
+        If Not apiKey.StartsWith("HDEV") Then
+            Return "Error: Invalid API key format."
+        End If
+
         Using client As New HttpClient()
             Dim url As String = $"https://api.henrikdev.xyz/valorant/v1/account/{playerName}/{playerTag}"
+            client.DefaultRequestHeaders.Add("Authorization", apiKey)
             Try
                 Dim response As HttpResponseMessage = Await client.GetAsync(url)
                 If response.IsSuccessStatusCode Then
@@ -25,9 +30,18 @@ Public Class Form1
         End Using
     End Function
 
-    Private Async Function GetValorantUsernameAsync(puuid As String) As Task(Of String)
+    Private Async Function GetValorantUsernameAsync(puuid As String, apiKey As String) As Task(Of String)
+        If String.IsNullOrWhiteSpace(apiKey) Then
+            Return "Error: No API key provided."
+        End If
+
+        If Not apiKey.StartsWith("HDEV") Then
+            Return "Error: Invalid API key format."
+        End If
+
         Using client As New HttpClient()
             Dim url As String = $"https://api.henrikdev.xyz/valorant/v1/by-puuid/account/{puuid}?force=true"
+            client.DefaultRequestHeaders.Add("Authorization", apiKey)
             Try
                 Dim response As HttpResponseMessage = Await client.GetAsync(url)
                 If response.IsSuccessStatusCode Then
@@ -43,8 +57,9 @@ Public Class Form1
         End Using
     End Function
 
-    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Submit.Click
         Dim input As String = TextInput.Text.Trim()
+        Dim apiKey As String = ApiKeyInput.Text.Trim()
         Dim output As String = String.Empty
 
         ' Regex pattern to match UUID format
@@ -53,12 +68,12 @@ Public Class Form1
         If input.Contains("#") Then
             Dim parts As String() = input.Split("#"c)
             If parts.Length = 2 Then
-                output = Await GetValorantPUUIDAsync(parts(0), parts(1))
+                output = Await GetValorantPUUIDAsync(parts(0), parts(1), apiKey)
             Else
                 output = "Invalid input format for username."
             End If
         ElseIf Regex.IsMatch(input, uuidPattern) Then
-            output = Await GetValorantUsernameAsync(input)
+            output = Await GetValorantUsernameAsync(input, apiKey)
         Else
             output = "Invalid input format for PUUID."
         End If
@@ -66,12 +81,11 @@ Public Class Form1
         TextOutput.Text = output
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Copy.Click
         If TextOutput.Text <> String.Empty Then
             Clipboard.SetText(TextOutput.Text)
         Else
             MessageBox.Show("There is no text to copy.", "Info")
         End If
     End Sub
-
 End Class
